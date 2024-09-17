388234414375fb61d42d20a9e2c5e54e9ac74f0a
ryanmac <ryan.maccarthy@gmail.com>
2024-09-17 14:27:05 -0500

    styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,14 +2,14 @@ import { Button } from "@/components/ui/button"
 
 export function Footer() {
   return (
-    <footer className="py-6 mt-12 bg-indigo-50 text-gray-800 dark:bg-gray-800 dark:text-gray-200">
+    <footer className="py-6 mt-12 bg-background text-gray-600 dark:bg-background dark:text-gray-600">
       <div className="container mx-auto px-8 flex justify-between items-center">
         {/* <Button variant="outline">Tip the Dev</Button> */}
         <div className="space-x-4 mx-auto">
-          <a href="/about" className="text-gray-600 dark:text-gray-200 hover:text-gray-900">About Us</a>
-          <a href="/feedback" className="text-gray-600 dark:text-gray-200 hover:text-gray-900 pl-8">Feedback</a>
-          <a href="/privacy" className="text-gray-600 dark:text-gray-200 hover:text-gray-900 pl-8">Privacy Policy</a>
-          <a href="/terms" className="text-gray-600 dark:text-gray-200 hover:text-gray-900 pl-8">Terms of Service</a>
+          <a href="/about" className="hover:text-gray-500">About Us</a>
+          <a href="/feedback" className="hover:text-gray-500 pl-8">Feedback</a>
+          <a href="/privacy" className="hover:text-gray-500 pl-8">Privacy Policy</a>
+          <a href="/terms" className="hover:text-gray-500 pl-8">Terms of Service</a>
         </div>
       </div>
     </footer>
